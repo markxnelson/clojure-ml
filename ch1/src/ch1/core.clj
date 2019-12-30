18822c786d4468f4b239ef004788faeedf637a1c
Mark Nelson <mark.x.nelson@oracle.com>
2019-12-30 18:17:25 -0500

    matrix add and eq

--- ch1/src/ch1/core.clj
+++ ch1/src/ch1/core.clj
@@ -1,5 +1,6 @@
 (ns ch1.core
   (:use clojure.core.matrix)
+  (:require [clojure.core.matrix.operators :as M])
   (:require [clatrix.core :as cl])
   (:gen-class))
 
@@ -7,7 +8,7 @@
   "Creates a square matrix of size n x n
   whose elements are all e"
   [n e & {:keys [implementation]
-          :or {implementation :persistent-vector}}]
+          :or   {implementation :persistent-vector}}]
   (let [repeater #(repeat n %)]
     (matrix implementation (-> e repeater repeater))))
 
@@ -42,6 +43,20 @@
   [n m]
   (compute-matrix [n m]
                   (fn [i j] (rand-int 100))))
+
+(defn matrix-eq
+  "Checks if two matrices are equal"
+  [A B]
+  (and (= (count A) (count B))
+       (reduce #(and %1 %2) (map = A B))))
+
+(defn matrix-add
+  "Add two or more matrices"
+  ([A B]
+   (mapv #(mapv + %1 %2) A B))
+  ([A B & more]
+   (let [M (concat [A B] more)]
+     (reduce matrix-add M))))
 
 (defn -main
   "I don't do a whole lot ... yet."
